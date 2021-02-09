3720237c3d4bf9476ee14a442633be5ff396c838
Kushagra Vaish <vaish.kushagra@gmail.com>
2021-02-09 20:27:08 +0530

    second commit

--- app/src/main/java/com/example/exoplayertest/MainActivity.java
+++ app/src/main/java/com/example/exoplayertest/MainActivity.java
@@ -2,9 +2,22 @@
 
 package com.example.exoplayertest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.google.android.exoplayer2.MediaItem;
 import com.google.android.exoplayer2.SimpleExoPlayer;
@@ -12,16 +25,72 @@ import com.google.android.exoplayer2.ui.StyledPlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int MY_CAMERA_REQUEST_CODE = 2012;
+    private WebView mWebRTCWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SimpleExoPlayer player = new SimpleExoPlayer.Builder(this).build();
-        StyledPlayerView playerView = findViewById(R.id.simpleTest);
-        playerView.setPlayer(player);
-        MediaItem mediaItem = MediaItem.fromUri("https://dyte-uploads.s3.ap-south-1.amazonaws.com/test/vp9_sample.webm");
-        player.setMediaItem(mediaItem);
-        player.prepare();
-        player.play();
+         mWebRTCWebView = (WebView) findViewById(R.id.webView);
+
+        setUpWebViewDefaults(mWebRTCWebView);
+        mWebRTCWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    request.grant(request.getResources());
+                }
+            }
+        });
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO, Manifest.permission.MODIFY_AUDIO_SETTINGS}, MY_CAMERA_REQUEST_CODE);
+        } else {
+            mWebRTCWebView.loadUrl("https://app.dyte.in/meeting/stage/rust-brie");
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
+                mWebRTCWebView.loadUrl("https://app.dyte.in/meeting/stage/rust-brie");
+            } else {
+                Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+
+        // Enable Javascript
+        settings.setJavaScriptEnabled(true);
+
+        // Use WideViewport and Zoom out if there is no viewport defined
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+
+        // Enable pinch to zoom without the zoom buttons
+        settings.setBuiltInZoomControls(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+
+        // Allow use of Local Storage
+        settings.setDomStorageEnabled(true);
+
+        // Hide the zoom controls for HONEYCOMB+
+        settings.setDisplayZoomControls(false);
+
+        // Enable remote debugging via chrome://inspect
+        WebView.setWebContentsDebuggingEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // AppRTC requires third party cookies to work
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
     }
 }
